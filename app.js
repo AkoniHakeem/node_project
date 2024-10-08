d5b167c1e164e5c444c40c090aef85c024d26f13
akonihakeem <akozymayor@gmail.com>
2024-10-08 15:19:26 +0100

    added new route

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // create node js server
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+/* 
+    function severHandle(req, res) {
     const url = req.url;
     const users = [
         {
@@ -33,9 +34,69 @@ const server = http.createServer((req, res) => {
         const devName = "Akoni Mayowa";
         res.end('Hello from' + ' ' + devName);
     }
+}
+
+*/
+
+function getUserAtIndex(index, users) {
+    const userAtIndex = users[index];
+    return userAtIndex;
+}
+
+function getIndexFromUrlString(url) {
+    const arrayOfStrings = url.split("/");
+    const index = arrayOfStrings[2];
+    return index;
+}
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const users = [
+        {
+            name: "Iliyas Hassan",
+            email: "hassan@gmail.com"
+        },
+        {
+            name: "Jamiu Olalekan",
+            email: "olalekan@gmail.com",
+        },
+        {
+            name: "Abdul Qudus Oyelami",
+            email: "oyelamiqudus@gmail.com"
+        }
+    ]
+    console.log(url);
+    if (url === "/user") {
+        res.end(`These are the registered users: ${JSON.stringify(users)}`);
+    }
+    else if (url.match(/^\/user\/\d$/)) {
+        console.log("this is trying to access a specific resource.");
+        const index = getIndexFromUrlString(url);
+        if (index > users.length - 1) {
+            res.end("Index greater than the available users");
+            return;
+        }
+        const userAtIndex = getUserAtIndex(index, users);
+        res.end(`You made a request with the path id: ${JSON.stringify(userAtIndex)}`);
+    }
+    else if (url.match(/^\/user\/\d\/get-email$/)) {
+        console.log("this is trying to access a specific resource.");
+        const index = getIndexFromUrlString(url);
+        if (index > users.length - 1) {
+            res.end("Index greater than the available users");
+            return;
+        }
+        const userAtIndex = getUserAtIndex(index, users);
+        const userEmail = userAtIndex.email;
+        res.end(`You made a request with the path id: ${index} and it fetched the email - ${userEmail} - of the matched user`);
+    }
+    else {
+        const devName = "Akoni Mayowa";
+        res.end('Hello from' + ' ' + devName);
+    }
 });
 
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(5100, () => {
+    console.log('Server is running on port 5100');
 });
